c8ae891d53f696686322a9ae63422e3583f14881
Carlos Torres <carlos.torres@despegar.com>
2018-06-04 11:41:48 -0300

    Se agrega configuracion para los drivers
    Se incorpora la validacion de SO

---
+++ src/main/java/Despegar/com/despegar/config/configuracion.java
@@ -0,0 +1,79 @@
+package Despegar.com.despegar.config;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+
+
+public class configuracion {
+	
+	protected static WebDriver driver;
+	private static final String CHROME_KEY = "webdriver.chrome.driver";
+    private static final String FIREFOX_KEY = "webdriver.gecko.driver";
+    private static final String IE_KEY = "webdriver.ie.driver";
+    private static final String EDGE_KEY = "webdriver.edge.driver";
+    private ChromeOptions chromeOptions;
+    protected WebDriverWait wait;
+    
+
+	@BeforeClass
+	public void setUp() throws Exception{
+		browser(browsers.CHROME);
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().fullscreen();
+	    wait = new WebDriverWait(driver,10);
+	}
+	
+	
+	private String osName() throws Exception {
+		String os = System.getProperty("os.name").toLowerCase();
+		if(os.contains("mac")) {
+			return "mac";
+		}
+		if(os.contains("nux") || os.contains("nix")) {
+			return "linux";
+		}
+		throw new Exception("No se puede ejecutar esta prueba en este sistema operativo.... "
+				+ "PD:  compre una  MAC");
+	}
+
+	private void browser(browsers browser) throws Exception {
+		switch (browser) {
+		case CHROME:
+			System.setProperty(CHROME_KEY, 
+					System.getProperty("user.dir") +
+					"/src/main/java/Despegar/com/despegar/drivers/" + osName() + "chromedriver");
+			
+		case FIREFOX:
+			System.setProperty(FIREFOX_KEY, 
+					System.getProperty("user.dir") + 
+					"/src/main/java/Despegar/com/despegar/drivers/" + osName() + "geckodriver");
+			
+		}
+		throw new Exception("El Browser seleccionando no esta disponible para esta prueba");	
+	}
+	
+	
+	private void cleanUp(){
+		driver.manage().deleteAllCookies();
+	}
+	
+	@AfterClass
+	public void tearDown(){
+		cleanUp();
+		driver.close();
+		driver.quit();
+	}
+	
+	
+	public enum browsers{
+		  CHROME,
+		  FIREFOX
+	}
+}
