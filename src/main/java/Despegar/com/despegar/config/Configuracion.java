b901cb6b09a2f1344740f09f69422c91ecc228e1
Carlos Torres <carlos.torres@despegar.com>
2018-06-04 12:03:49 -0300

    Cambios para la ejecucion

--- src/main/java/Despegar/com/despegar/config/Configuracion.java
+++ src/main/java/Despegar/com/despegar/config/Configuracion.java
@@ -4,13 +4,14 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 
 
-public class configuracion {
+public class Configuracion {
 	
 	protected static WebDriver driver;
 	private static final String CHROME_KEY = "webdriver.chrome.driver";
@@ -24,7 +25,6 @@ public class configuracion {
 	@BeforeClass
 	public void setUp() throws Exception{
 		browser(browsers.CHROME);
-		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().fullscreen();
 	    wait = new WebDriverWait(driver,10);
@@ -48,15 +48,22 @@ public class configuracion {
 		case CHROME:
 			System.setProperty(CHROME_KEY, 
 					System.getProperty("user.dir") +
-					"/src/main/java/Despegar/com/despegar/drivers/" + osName() + "chromedriver");
+					"/src/main/java/Despegar/com/despegar/drivers/" + osName() + "/chromedriver");
+			driver = new ChromeDriver();
+			break;
 			
 		case FIREFOX:
 			System.setProperty(FIREFOX_KEY, 
 					System.getProperty("user.dir") + 
-					"/src/main/java/Despegar/com/despegar/drivers/" + osName() + "geckodriver");
+					"/src/main/java/Despegar/com/despegar/drivers/" + osName() + "/geckodriver");
+			driver= new FirefoxDriver();
+			break;
+			
+			default: 
+				throw new Exception("El Browser seleccionando no esta disponible para esta prueba");
 			
 		}
-		throw new Exception("El Browser seleccionando no esta disponible para esta prueba");	
+	
 	}
 	
 	
