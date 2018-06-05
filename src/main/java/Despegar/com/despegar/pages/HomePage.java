00cd50ed1c6a4c4d1846d0d6e85a221996c6ef9a
Carlos Torres <carlos.torres@despegar.com>
2018-06-05 09:27:36 -0300

    Cambios en las configuraciones de las pruebas

--- src/main/java/Despegar/com/despegar/pages/HomePage.java
+++ src/main/java/Despegar/com/despegar/pages/HomePage.java
@@ -4,65 +4,63 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class HomePage  extends PageWeb{
-	
-	@FindBy(xpath="//*[contains(@class, 'nevo-header-navigation-menu-item nevo-js-header-menu-item')]"
+public class HomePage extends PageWeb {
+
+	@FindBy(xpath = "//*[contains(@class, 'nevo-header-navigation-menu-item nevo-js-header-menu-item')]"
 			+ "//*[@class='nevo-header-navigation-menu-text' and text()='Passagens' or text()='Vuelos']")
 	private WebElement vuelos;
-	
-	@FindBy(xpath= "//*[@class='nevo-header-navigation-menu-item nevo-js-header-menu-item HOTELS nevo--active']")
+
+	@FindBy(xpath = "//*[@class='nevo-header-navigation-menu-item nevo-js-header-menu-item HOTELS nevo--active']")
 	private WebElement alojamiento;
-	
-	@FindBy(xpath= "//*[@class='nevo-header-navigation-menu-item nevo-js-header-menu-item PACKAGES']")
+
+	@FindBy(xpath = "//*[@class='nevo-header-navigation-menu-item nevo-js-header-menu-item PACKAGES']")
 	private WebElement paquetes;
-	
-	@FindBy(xpath= "//[@class='nevo-header-navigation-menu-item nevo-js-header-menu-item CARS']")
+
+	@FindBy(xpath = "//[@class='nevo-header-navigation-menu-item nevo-js-header-menu-item CARS']")
 	private WebElement autos;
-	
-	
 
 	public HomePage(WebDriver driver) {
 		super(driver);
 	}
-	
-	
+
 	/**
 	 * Este metodo lleva al formulario de busqueda de Alojamientos.
 	 * 
-	 * @return {@link Alojamientos}  Pagina de alojamientos inicializada
+	 * @return {@link Alojamientos} Pagina de alojamientos inicializada
 	 */
 	public Alojamientos clickOnAlojamientos() {
 		alojamiento.click();
 		return new Alojamientos(driver);
 	}
-	
+
 	/**
 	 * Este metodo lleva al formulario de Autos
+	 * 
 	 * @return {@link Autos} Pagina de autos
 	 */
 	public Autos clickOnAutos() {
 		autos.click();
 		return new Autos(driver);
 	}
-	
+
 	/**
 	 * Este metodo lleva al formulario de Vuelos
+	 * 
 	 * @return {@link Vuelos} Pagina de vuelos
 	 */
 	public Vuelos clickOnVuelos() {
 		vuelos.click();
 		return new Vuelos(driver);
 	}
-	
+
 	/**
 	 * Este metodo lleva al formulario de Paquetes
+	 * 
 	 * @return {@link Paquetes} Pagina de Paquetes
 	 */
 	public Paquetes clickOnPaquetes() {
 		paquetes.click();
 		return new Paquetes(driver);
 	}
-	
-	
 
 }
--- src/main/java/Despegar/com/despegar/pages/PageWeb.java
+++ src/main/java/Despegar/com/despegar/pages/PageWeb.java
@@ -1,5 +1,6 @@
 package Despegar.com.despegar.pages;
 
+import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,67 +8,119 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.CalendarUtil;
 
+/**
+ * Clase Principal de Page Object
+ *
+ */
 public class PageWeb {
-	
-	@FindBy(xpath="//*[@class='sbox-button-default sbox-max-width-container']"
+
+	@FindBy(xpath = "//*[@class='sbox-button-default sbox-max-width-container']"
 			+ "//*[@class='sbox-3-btn -secondary -md sbox-search']")
 	protected WebElement buscarBtn;
-	
-	  @FindBy(xpath = "//*[@class='input-container sbox-checkin-date-container']")
-	  protected WebElement entryDate;
-	  
-	  @FindBy(className = "_dpmg2--controls-next")
-	  protected WebElement nextMonth;
-	
+
+	@FindBy(xpath = "//*[@class='input-container sbox-checkin-date-container']")
+	protected WebElement entryDate;
+
+	@FindBy(className = "_dpmg2--controls-next")
+	protected WebElement nextMonth;
+
+	@FindBy(xpath = ".//*[@class='ac-group-items' or @class='geo-autocomplete-list']")
+	private WebElement autoCompleteList;
+
 	protected WebDriver driver;
-	
-	
+
+	/**
+	 * Constructor
+	 * 
+	 * @param driver
+	 */
 	public PageWeb(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
 
-	
 	private void setDateEntry(String dateEntry) throws Exception {
-	    CalendarUtil calendar = new CalendarUtil(dateEntry);
-	    entryDate.click();
-	    for (int i = 0; i < calendar.getMonth(); i++) {
-	      if (calendar.isIncrement()) {
-	        nextMonth.click();
-	      }
-	    }
-	        driver.findElement(By.xpath("//div[" + (calendar.getMonth() + 1) + "]/div[4]/span["
-	    + calendar.getDay() + "]")).click();
-	  }
-	
+		CalendarUtil calendar = new CalendarUtil(dateEntry);
+		entryDate.click();
+		for (int i = 0; i < calendar.getMonth(); i++) {
+			if (calendar.isIncrement()) {
+				nextMonth.click();
+			}
+		}
+		driver.findElement(By.xpath("//div[" + (calendar.getMonth() + 1) + "]/div[4]/span[" + calendar.getDay() + "]"))
+				.click();
+	}
+
 	private void setDateDeparture(String dateEntry, String dateDeparture) throws Exception {
-	    CalendarUtil calendar = new CalendarUtil(dateEntry);
-	    int monthEntry = calendar.getMonth();
-	    int dayEntry = calendar.getDay();
-	    calendar = new CalendarUtil(dateDeparture);
-	    int monthDeparture = calendar.getMonth();
-	    int dayDeparture = calendar.getDay();
+		CalendarUtil calendar = new CalendarUtil(dateEntry);
+		int monthEntry = calendar.getMonth();
+		int dayEntry = calendar.getDay();
+		calendar = new CalendarUtil(dateDeparture);
+		int monthDeparture = calendar.getMonth();
+		int dayDeparture = calendar.getDay();
 
-	    if (monthDeparture - monthEntry == 0 && dayDeparture > dayEntry) {
-	      driver.findElement(By.xpath("//div[" + (calendar.getMonth() + 1) + "]/div[4]/span[" + calendar.getDay() + "]")).click();
-	      return;
-	    }
+		if (monthDeparture - monthEntry == 0 && dayDeparture > dayEntry) {
+			driver.findElement(
+					By.xpath("//div[" + (calendar.getMonth() + 1) + "]/div[4]/span[" + calendar.getDay() + "]"))
+					.click();
+			return;
+		}
 
-	    if (monthDeparture - monthEntry == 1 && dayDeparture < dayEntry) {
-	      for (int i = 0; i < (monthDeparture - monthEntry); i++) {
-	        if (calendar.isIncrement()) {
-	          nextMonth.click();
-	        }
-	      }
-	      driver.findElement(By.xpath(
-	          "//div[" + (calendar.getMonth() + 1) + "]/div[4]/span[" + calendar.getDay() + "]")).click();
-	    } else {
-	      throw new Exception("No se pueden realizar una reserva por mas de 1 mes....");
-	    }
-	  }
-	
-	protected void fechas(String dateEntry,String dateDeparture) throws Exception {
+		if (monthDeparture - monthEntry == 1 && dayDeparture < dayEntry) {
+			for (int i = 0; i < (monthDeparture - monthEntry); i++) {
+				if (calendar.isIncrement()) {
+					nextMonth.click();
+				}
+			}
+			driver.findElement(
+					By.xpath("//div[" + (calendar.getMonth() + 1) + "]/div[4]/span[" + calendar.getDay() + "]"))
+					.click();
+		} else {
+			throw new Exception("No se pueden realizar una reserva por mas de 1 mes....");
+		}
+	}
+
+	/**
+	 * Metodo que selecciona las Fechas del calendario
+	 * 
+	 * @param dateEntry
+	 * @param dateDeparture
+	 * @throws Exception
+	 */
+	protected void fechas(String dateEntry, String dateDeparture) throws Exception {
 		setDateEntry(dateEntry);
 		setDateDeparture(dateEntry, dateDeparture);
+	}
+
+	/**
+	 * Metodo para seleccionar una opcion de la lista desplegable
+	 * 
+	 * @param list
+	 *            Lista de Opciones
+	 * @param option
+	 *            String opcion que se quiere seleccionar
+	 * @return Elemento que contiene la opcion
+	 * @throws Exception
+	 */
+	protected WebElement selectOption(List<WebElement> list, String option) throws Exception {
+		if (!list.isEmpty() && !option.isEmpty()) {
+			for (WebElement webElement : list) {
+				if (webElement.getText().toUpperCase().contains(option.toUpperCase())) {
+					return webElement;
+				}
+			}
+		}
+		throw new Exception("Error al querer seleccionar un Elemento de Lista");
+	}
+
+	/**
+	 * Devueleve la Lista desplegable de Origen y destino
+	 * 
+	 * @return Lista de elementos
+	 * @throws Exception
+	 */
+	protected List<WebElement> getListCitiesOrAirports() throws Exception {
+		Thread.sleep(2000);
+		return autoCompleteList.findElements(By.tagName("li"));
 	}
 }
