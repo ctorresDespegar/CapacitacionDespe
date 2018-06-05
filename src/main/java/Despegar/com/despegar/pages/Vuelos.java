1ed97da85f293e479d347d65a5435eb107717e81
Carlos Torres <carlos.torres@despegar.com>
2018-06-05 09:27:06 -0300

    Cambios de Xpath en vuelos

--- src/main/java/Despegar/com/despegar/pages/Vuelos.java
+++ src/main/java/Despegar/com/despegar/pages/Vuelos.java
@@ -5,35 +5,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class Vuelos extends PageWeb{
-	
+public class Vuelos extends PageWeb {
+
 	@FindBy(xpath = "//*[@class='radio-circle sbox-radio-circle']")
 	private List<WebElement> opcionsFlights;
-	
+
 	@FindBy(xpath = "//*[@class='input-tag sbox-main-focus sbox-bind-reference-flight-roundtrip-origin-input "
 			+ "sbox-primary sbox-places-first places-inline']")
 	private WebElement origen;
-	
+
 	@FindBy(xpath = "//*[@class='input-tag sbox-main-focus sbox-bind-reference-flight-roundtrip-destination-input"
 			+ " sbox-secondary sbox-places-second places-inline']")
 	private WebElement destino;
-	
-
 
 	public Vuelos(WebDriver driver) {
 		super(driver);
 	}
 
-	
-	
-	
-	public ResultadosVuelos busqueda(String origin,String destination, String fechaSalida, String fechaRetorno) throws Exception {
+	public ResultadosVuelos busqueda(String origin, String destination, String fechaSalida, String fechaRetorno)
+			throws Exception {
+		origen.clear();
 		origen.sendKeys(origin);
+		selectOption(getListCitiesOrAirports(), origin);
+		destino.clear();
 		destino.sendKeys(destination);
+		selectOption(getListCitiesOrAirports(), destination);
 		fechas(fechaSalida, fechaRetorno);
 		buscarBtn.click();
 		return new ResultadosVuelos(driver);
 	}
-	
-	
+
 }
