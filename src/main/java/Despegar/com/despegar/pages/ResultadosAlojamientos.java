0cfbf6d48ea4a74b44b34cff18a4c7227d5f1abb
Mayra Perez <mayra.perez@despegar.com>
2018-06-06 11:42:04 -0300

    Cambios en page y test. parte 2000

--- src/main/java/Despegar/com/despegar/pages/ResultadosAlojamientos.java
+++ src/main/java/Despegar/com/despegar/pages/ResultadosAlojamientos.java
@@ -1,11 +1,22 @@
 package Despegar.com.despegar.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class ResultadosAlojamientos extends PageWeb{
+	
+	@FindBy (id = "mapResult")
+	private WebElement mapa;
 
 	public ResultadosAlojamientos(WebDriver driver) {
 		super(driver);
 	}
+
+	public boolean verMapaSideBar() {
+		mapa.isDisplayed();
+		return true;
+	}
+
 
 }
--- src/main/java/Despegar/com/despegar/test/AlojamientosTest.java
+++ src/main/java/Despegar/com/despegar/test/AlojamientosTest.java
@@ -1,21 +1,28 @@
 package Despegar.com.despegar.test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
+
+import com.sun.xml.internal.ws.protocol.soap.VersionMismatchException;
 
 import Despegar.com.despegar.config.Configuracion;
 import Despegar.com.despegar.pages.Alojamientos;
 import Despegar.com.despegar.pages.HomePage;
-import Despegar.com.despegar.pages.Vuelos;
+import Despegar.com.despegar.pages.ResultadosAlojamientos;
 
-public class AlojamientosTest extends Configuracion{
-	
-	@Test 
+public class AlojamientosTest extends Configuracion {
+
+	@Test
 	public void testAlojamiento() throws Exception {
 		HomePage home = new HomePage(driver);
 		Alojamientos alojamientos = home.clickOnAlojamientos();
 		alojamientos.busquedaAlojamiento("Roma", "16/08/2018", "19/08/2018");
+		ResultadosAlojamientos alojamientosResult = new ResultadosAlojamientos(driver);
+		WebElement mapaElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("mapResult")));
 		
-		
+		//alojamientosResult.verMapaSideBar();
 	}
 
 }
