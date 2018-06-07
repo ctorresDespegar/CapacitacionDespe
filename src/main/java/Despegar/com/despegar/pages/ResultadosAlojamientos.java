467062de02e9f28651ff3618e5ff58c15f97c043
Mayra Perez <mayra.perez@despegar.com>
2018-06-07 16:28:40 -0300

    Subo método para llamar al orden por precio menor a mayor.

--- src/main/java/Despegar/com/despegar/pages/ResultadosAlojamientos.java
+++ src/main/java/Despegar/com/despegar/pages/ResultadosAlojamientos.java
@@ -1,29 +1,34 @@
 package Despegar.com.despegar.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
-public class ResultadosAlojamientos extends PageWeb{
-	
-	@FindBy (id = "mapResult")
-	private WebElement mapa;
-	
-	@FindBy (id = "sorting")
+public class ResultadosAlojamientos extends PageWeb {
+
+	@FindBy(xpath = "//*[@id=\"sorting\"]")
 	private WebElement ordenarPor;
+
+	@FindBy(xpath = "//[@value='price_ascending'")
+	private WebElement precioAscendiente;
 
 	public ResultadosAlojamientos(WebDriver driver) {
 		super(driver);
 	}
 
-	public boolean verMapaSideBar() {
-		mapa.isDisplayed();
-		return true;
-	}
-	
-	public void ordenarPorDropdown () {
+	public void ordernarDropDownClick() {
 		ordenarPor.click();
 	}
 
+	public void ordenarPorDropdown() {
+		ordenarPor.isDisplayed();
+	}
+
+	public void selectComboBox(String texto) {
+		Select comboBox = new Select(ordenarPor);
+		comboBox.selectByVisibleText(texto);
+	}
 
 }
--- src/main/java/Despegar/com/despegar/test/AlojamientosTest.java
+++ src/main/java/Despegar/com/despegar/test/AlojamientosTest.java
@@ -1,5 +1,8 @@
 package Despegar.com.despegar.test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 import Despegar.com.despegar.config.Configuracion;
 import Despegar.com.despegar.pages.Alojamientos;
@@ -13,10 +16,10 @@ public class AlojamientosTest extends Configuracion {
 		HomePage home = new HomePage(driver);
 		Alojamientos alojamientos = home.clickOnAlojamientos();
 		alojamientos.busquedaAlojamiento("Roma", "16/08/2018", "19/08/2018");
-		//alojamientos.buscarButtonResultados();
-		//ResultadosAlojamientos alojamientosResult = new ResultadosAlojamientos(driver);
-		//WebElement mapaElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("mapResult")));
-		Thread.sleep(5000);
+		ResultadosAlojamientos alojamientosResult = new ResultadosAlojamientos(driver);
+		alojamientosResult.ordenarPorDropdown();
+		alojamientosResult.selectComboBox("Precio: menor a mayor");
+		
 		//WebElement ordenarPor = wait.until(ExpectedConditions.visibilityOfElementLocated((By.id("sorting"))));
 		//alojamientosResult.ordenarPorDropdown();
 		//alojamientosResult.verMapaSideBar();
