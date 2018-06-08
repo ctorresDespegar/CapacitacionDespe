201b107bc27c3b29912b96b05db961c1c4839e9d
maximilianograjales <maximiliano.grajales@despegar.com>
2018-06-08 11:28:25 -0300

    DataProvider

--- src/main/java/Despegar/com/despegar/test/Test1.java
+++ src/main/java/Despegar/com/despegar/test/Test1.java
@@ -1,5 +1,6 @@
 package Despegar.com.despegar.test;
 
+import java.util.Map;
 import org.testng.annotations.Test;
 import Despegar.com.despegar.config.Configuracion;
 import Despegar.com.despegar.pages.HomePage;
@@ -8,12 +9,12 @@ import Despegar.com.despegar.pages.Vuelos;
 
 public class Test1 extends Configuracion {
 	
-	@Test
-	public void prueba() throws Exception {
+	@Test(dataProvider = "probando", dataProviderClass = DataProviderPrueba.class)
+	public void prueba(Map<String,String> map) throws Exception {
+		String origen = map.get("origen");
+		String destino = map.get("destino");
 		HomePage home = new HomePage(driver);
 		Vuelos vuelos = home.clickOnVuelos();
-		vuelos.busqueda("Roma", "Madrid", "16/08/2018", "16/08/2018");
-		
+		vuelos.busqueda(origen, destino, "16/08/2018", "16/08/2018");
 	}
-
 }
