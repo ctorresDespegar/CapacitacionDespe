3bdf228e1071cda89b2b7a1ae61b9319bfa22f62
Mayra Perez <mayra.perez@despegar.com>
2018-06-11 09:56:04 -0300

    agrego pasos al test

--- src/main/java/Despegar/com/despegar/test/AlojamientosTest.java
+++ src/main/java/Despegar/com/despegar/test/AlojamientosTest.java
@@ -1,5 +1,7 @@
 package Despegar.com.despegar.test;
 
+import java.util.Map;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -11,11 +13,14 @@ import Despegar.com.despegar.pages.ResultadosAlojamientos;
 
 public class AlojamientosTest extends Configuracion {
 
-	@Test
-	public void testAlojamiento() throws Exception {
+	@Test(dataProvider = "alojamiento", dataProviderClass = DataProviderPrueba.class)
+	public void testAlojamiento (Map<String, String> map) throws Exception {
+		String origen = map.get("origen");
+		String fechaIda = map.get("fechaIda");
+		String fechaVuelta = map.get("fechaVuelta");
 		HomePage home = new HomePage(driver);
 		Alojamientos alojamientos = home.clickOnAlojamientos();
-		alojamientos.busquedaAlojamiento("Roma", "16/08/2018", "19/08/2018");
+		alojamientos.busquedaAlojamiento();
 		ResultadosAlojamientos alojamientosResult = new ResultadosAlojamientos(driver);
 		alojamientosResult.ordenarPorDropdown();
 		alojamientosResult.selectComboBox("Precio: menor a mayor");
